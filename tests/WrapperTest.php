db717234b8b6136a8c720bac650b8bb13f32502f
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-04-09 17:12:02 +0200

    Use tests from the new JSON file

--- tests/WrapperTest.php
+++ tests/WrapperTest.php
@@ -6,25 +6,51 @@ use PHPUnit\Framework\TestCase;
 
 final class WrapperTest extends TestCase
 {
-    /**
-     * @dataProvider \MessagePack\Tests\DataProvider::provideData
-     * @param mixed $title
-     * @param mixed $raw
-     * @param mixed $packed
-     */
-    public function testPack(string $title, $raw, string $packed)
+    public function provideData()
     {
-        self::assertEquals($packed, msgpack_pack($raw), $title);
+        /** @var array $json */
+        $json = json_decode(file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'msgpack-test-suite.json'), true);
+        foreach ($json as $fileName => $fileItems) {
+            foreach ($fileItems as $index => $item) {
+                if (in_array(key($item), ['timestamp', 'ext'], true)) {
+                    continue;
+                }
+
+                $possibleResults = [];
+                foreach ($item['msgpack'] as $possibleResult) {
+                    $possibleResults[] = hex2bin(str_replace('-', '', $possibleResult));
+                }
+                yield [
+                    $fileName . ' #' . $index . '(' . key($item) . ')',
+                    current($item),
+                    $possibleResults,
+                ];
+            }
+        }
     }
 
     /**
-     * @dataProvider \MessagePack\Tests\DataProvider::provideData
-     * @param mixed $title
-     * @param mixed $raw
-     * @param mixed $packed
+     * @dataProvider provideData
+     * @param string $title
+     * @param mixed  $input
+     * @param array  $possibilities
      */
-    public function testUnpack(string $title, $raw, string $packed)
+    public function testPack(string $title, $input, array $possibilities)
     {
-        self::assertEquals($raw, msgpack_unpack($packed), $title);
+        self::assertTrue(in_array(msgpack_pack($input), $possibilities, true), $title);
+    }
+
+    /**
+     * @dataProvider provideData
+     * @param string $title
+     * @param mixed  $input
+     * @param array  $possibilities
+     */
+    public function testUnpack(string $title, $input, array $possibilities)
+    {
+        foreach ($possibilities as $key => $value) {
+            $possibilities[$key] = msgpack_unpack($value);
+        }
+        self::assertTrue(in_array($input, $possibilities, true), $title);
     }
 }
